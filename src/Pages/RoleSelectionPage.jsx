fc0f7a24cbf55cad463dbda43dedb268352122f3
Nikolay Klimkovich <kolyaklimk@gmail.com>
2025-04-10 23:43:01 +0300

    update

--- src/Pages/RoleSelectionPage.jsx
+++ src/Pages/RoleSelectionPage.jsx
@@ -1,11 +1,19 @@
 ﻿import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function RoleSelectionPage({ onRoleChange }) {
+    const navigate = useNavigate();
+
+    const handleRoleChangeAndRedirect = async (newRole) => {
+        await onRoleChange(newRole);
+        navigate("/");
+    };
+
     return (
         <div>
             <h2>Кто вы?</h2>
-            <button onClick={() => onRoleChange('client')}>Клиент</button>
-            <button onClick={() => onRoleChange('specialist')}>Специалист</button>
+            <button onClick={() => handleRoleChangeAndRedirect('client')}>Клиент</button>
+            <button onClick={() => handleRoleChangeAndRedirect('specialist')}>Специалист</button>
         </div>
     );
 }
