77c84f5273b3fc9a3a21e8e74d75ddbe0194742a
Nikolay Klimkovich <kolyaklimk@gmail.com>
2025-04-17 19:46:33 +0300

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -44,7 +44,7 @@ function AppRouter({ telegramId, handleRoleChange, role, profileLink, apiUrl }) 
             setRole(data.role);
             console.log(data.role);
             localStorage.setItem('userRole', data.role);
-            navigate(`/profile/${specialistTelegramId}`);
+            navigate(`/profile/${123}`);
 
         } catch (error) {
             console.error("Error fetching user:", error);
@@ -62,7 +62,7 @@ function AppRouter({ telegramId, handleRoleChange, role, profileLink, apiUrl }) 
             <Route path="/change-role" element={<RoleSelectionPage onRoleChange={handleRoleChange} />} />
             <Route path="/archive" element={<ArchivePage />} />
             <Route path="/book-appointment" element={<BookAppointmentPage />} />
-            <Route path="/profile/:telegramId" element={<ProfilePage specialistTelegramId={specialistTelegramId} telegramId={telegramId} />} />
+            <Route path="/profile/:telegramId" element={<ProfilePage />} />
             <Route path="/schedule" element={<SchedulePage />} />
             <Route path="/appointments" element={<AppointmentsPage />} />
             <Route path="/subscription" element={<SubscriptionPage />} />
