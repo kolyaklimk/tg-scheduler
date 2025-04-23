cd3bfe0a59152e6a8379a6959ef04ed15c101924
Nikolay Klimkovich <kolyaklimk@gmail.com>
2025-04-23 23:47:27 +0300

    Update SchedulePage.jsx

--- src/Pages/Specialist/SchedulePage.jsx
+++ src/Pages/Specialist/SchedulePage.jsx
@@ -22,7 +22,10 @@ function SchedulePage({ telegramId, apiUrl }) {
                 if (response.ok) {
                     const data = await response.json();
                     setScheduledDates(data.map(date => new Date(date)));
+                    console.error("data");
+                    console.error(data);
                     console.error(ScheduledDates);
+                    console.error("data");
                 } else {
                     console.error("Error fetching scheduled dates:", response.status);
                 }
