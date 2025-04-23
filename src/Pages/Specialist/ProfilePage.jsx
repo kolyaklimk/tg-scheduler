5a641cbd37787f5b06ad2666fa87d866f88a1778
Nikolay Klimkovich <kolyaklimk@gmail.com>
2025-04-23 22:53:58 +0300

    Update ProfilePage.jsx

--- src/Pages/Specialist/ProfilePage.jsx
+++ src/Pages/Specialist/ProfilePage.jsx
@@ -1,6 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import './ProfilePage.css';
 
 function ProfilePage() {
     const { telegramId } = useParams();
