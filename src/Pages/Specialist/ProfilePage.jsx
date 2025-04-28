dbf1cee45f95d6b456873c7d3a3bcaba81587f03
Nikolay Klimkovich <kolyaklimk@gmail.com>
2025-04-28 23:29:10 +0300

    Update ProfilePage.jsx

--- src/Pages/Specialist/ProfilePage.jsx
+++ src/Pages/Specialist/ProfilePage.jsx
@@ -1,5 +1,5 @@
 ﻿import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import './ProfilePage.css';
 
 function ProfilePage() {
