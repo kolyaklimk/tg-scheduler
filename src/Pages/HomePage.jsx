2940b0500e47e73eecbd08c803e12d0581dcfff4
Nikolay Klimkovich <kolyaklimk@gmail.com>
2025-05-04 23:59:51 +0300

    Update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -83,9 +83,6 @@ function HomePage({ role, telegramId, apiUrl }) {
 
     return (
         <div>
-            <h1>Главная страница</h1>
-            <p>Ваша роль: {role}</p>
-
             {role === 'client' ? (
                 <div>
                     <h2>Ваши активные записи</h2>
