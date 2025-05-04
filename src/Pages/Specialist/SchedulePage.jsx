030632177719c9a6ff2c47fa9eba05020c76c726
Nikolay Klimkovich <kolyaklimk@gmail.com>
2025-05-05 00:55:26 +0300

    Update SchedulePage.jsx

--- src/Pages/Specialist/SchedulePage.jsx
+++ src/Pages/Specialist/SchedulePage.jsx
@@ -1,6 +1,6 @@
 ﻿import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { DatePicker } from '@mantine/dates';
+import { DatePicker, TimeInput } from '@mantine/dates';
 import {
     Container,
     Stack,
